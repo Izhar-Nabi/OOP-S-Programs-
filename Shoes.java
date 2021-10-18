d593447cdc7576824777a177de01f7042ee9d7ce
Izhar-Zour <90437474+Izhar-Nabi@users.noreply.github.com>
2021-10-19 00:34:47 +0500

    The Shoes Company Using Object

---
+++ Shoes.java
@@ -0,0 +1,19 @@
+class Shoes
+{
+	String Company;
+	String colour;
+	int pair;
+	String function;
+	public static void main(String args[]){
+	Shoes JLong= new Shoes();
+	System.out.println("\t\t !!WELCOME TO JLONG SHOES COMPANY !!");
+	JLong.Company="J-LONG";
+	JLong.colour="Black, Blue, Light Black And Brown are Available ";
+	JLong.pair=2;
+	JLong.function="For Waking, Joging, offices and also For Plays Games Shoes are Available";
+	System.out.println("Company Name is : "+JLong.Company );
+	System.out.println("colour Of Shoes Available :"+JLong.colour );
+	System.out.println("Paired Shoes :"+JLong.pair );	
+	System.out.println("Every Type Of Shoes is Available :"+JLong.function );
+	} 
+}
